31923baeda66da2d799ccaa3d29637b18f4c8b37
chandru1236 <chandrur23cb@psnacet.edu.in>
2026-07-07 18:02:03 +0530

    fix the crocs

--- backend/src/main/java/com/medisphere/config/CorsConfig.java
+++ backend/src/main/java/com/medisphere/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         CorsConfiguration configuration = new CorsConfiguration();
 
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://medi-sphere-info.vercel.app"
         ));
 
         configuration.setAllowedMethods(List.of(
@@ -29,6 +30,8 @@ public class CorsConfig {
         ));
 
         configuration.setAllowedHeaders(List.of("*"));
+
+        configuration.setExposedHeaders(List.of("Authorization"));
 
         configuration.setAllowCredentials(true);
 
